9f9ec1f796728ee321dc71f3618f8627ce7055be
nguyenngochuy00 <ngochuy25120@gmail.com>
2025-11-13 14:36:48 +0700

    Update custom module ytv_contract_analytic_accounts

--- ytv_contract_analytic_accounts/__manifest__.py
+++ ytv_contract_analytic_accounts/__manifest__.py
@@ -1,10 +1,10 @@
 {
-    'name': 'YTV Contract Analytic Accounts',
+    'name': 'YTV Contract Analytic Accounts', 
     'version': '18.0.1.0.0',
     'summary': 'Custom Many2many Analytic Accounts field to HR Contract',
-    'category': 'Payroll',
+    'category': 'Payroll', 
     'author': 'YTV-QTHT-Huy',
-    'depends': ['hr_payroll_account'],
+    'depends': ['hr_payroll_account', 'analytic'], 
     'data': [
         'views/hr_contract_views.xml',
     ],
--- ytv_contract_analytic_accounts/models/hr_contract.py
+++ ytv_contract_analytic_accounts/models/hr_contract.py
@@ -4,7 +4,7 @@ class HrContract(models.Model):
     _inherit = 'hr.contract'
     _description = 'Employee Contract'
 
-    analytic_account_ids = fields.Many2many(
+    x_analytic_account_ids = fields.Many2many(
         'account.analytic.account',
         'hr_contract_analytic_rel',
         'contract_id',
