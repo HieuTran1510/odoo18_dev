c35661ee0a71a2632d5a250023f52d1b2ee27e89
Hieu Tran <ytv709001@gmail.com>
2025-11-15 09:03:32 +0700

    Add function merger children record when merge company

--- ytv_partner_sharing/models/res_partner.py
+++ ytv_partner_sharing/models/res_partner.py
@@ -16,3 +16,29 @@ class ResPartner(models.Model):
         string='Công ty chia sẻ',
         help="Các công ty được phép truy cập đối tác này."
     )
+
+@api.model
+    def write(self, vals):
+        res = super(ResPartner, self).write(vals)
+
+        fields_to_sync = ['x_owner_company_id', 'x_shared_company_ids']
+
+        # Nếu có field nào cần đồng bộ được thay đổi
+        if any(f in vals for f in fields_to_sync):
+            for partner in self:
+                # Chỉ đồng bộ cho child records (liên hệ con)
+                children = self.search([('parent_id', '=', partner.id)])
+
+                # Đồng bộ x_owner_company_id
+                if 'x_owner_company_id' in vals:
+                    children.write({
+                        'x_owner_company_id': vals['x_owner_company_id']
+                    })
+
+                # Đồng bộ x_shared_company_ids (Many2many)
+                if 'x_shared_company_ids' in vals:
+                    children.write({
+                        'x_shared_company_ids': vals['x_shared_company_ids']
+                    })
+
+        return res
